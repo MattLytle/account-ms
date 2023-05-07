17e1a3616638e1d45c892d7e745c3f42e45dff40
MattLytle <dev@mattlytle.com>
2023-05-07 20:08:12 +0200

    MLACC-5: Separate Startup from Program

--- AccountMS/AccountMS/Program.cs
+++ AccountMS/AccountMS/Program.cs
@@ -1,40 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
 
 namespace AccountMS
 {
+    [ExcludeFromCodeCoverage(Justification = "It is not necessary to test application startup")]
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(o =>
-                {
-                    o.DocumentTitle = "AccountMS Swagger Page";
-                });
-            }
-
-            // TODO replace this later for a proper health check
-            app.MapGet("/health", () => "ok");
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ AccountMS/AccountMS/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AccountMS
+{
+    [ExcludeFromCodeCoverage(Justification = "It is not necessary to test application startup")]
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Account Microservice", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AccontMS v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/health", () => "ok");
+            });
+        }
+    }
+}
